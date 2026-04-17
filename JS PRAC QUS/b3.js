1c79ad729af05790de9f2eb77a0249a32f363826
TAABISH ALAM <100014087+TAABISH379@users.noreply.github.com>
2026-04-17 20:35:42 +0530

    basic

--- JS PRAC QUS/b3.js
+++ JS PRAC QUS/b3.js
@@ -1,1 +1,11 @@
 // B3 (Easy): Create variables of types number, string, boolean, null, undefined; log typeof for each.
+
+let a = 6;
+let b = "cat";
+let c = true;
+let d = null;
+console.log(typeof a); // "number"
+console.log(typeof b); // "string"
+console.log(typeof c); // "boolean"
+console.log(typeof d); // "object"  (historical JS quirk)
+console.log(typeof notDefined); // "undefined"
