453a24fbf862b05b8862ccf9edeb1be0742387ed
TAABISH ALAM <100014087+TAABISH379@users.noreply.github.com>
2026-04-18 20:53:40 +0530

    update submodule

---
+++ JS PRAC QUS/op1.js
@@ -0,0 +1,6 @@
+// Predict the output:
+
+console.log(null + 1); //1
+console.log("5" + 3);  //53
+console.log("5" - 3);  //2
+console.log(true + false);  //1 
---
+++ JS PRAC QUS/tf.js
@@ -0,0 +1,6 @@
+// Truthy or Falsy?
+
+console.log(Boolean(0)); // falsy
+console.log(Boolean("0")); // truthy
+console.log(Boolean([])); // truthy
+console.log(Boolean(undefined));// falsy
