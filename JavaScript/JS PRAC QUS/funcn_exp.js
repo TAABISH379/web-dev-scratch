1043417ea23cb8cf7eaecc4a1bdb5b4a52fc24d1
TAABISH ALAM <100014087+TAABISH379@users.noreply.github.com>
2026-04-21 22:22:21 +0530

    spread operator

---
+++ JavaScript/JS PRAC QUS/funcn_exp.js
@@ -0,0 +1,7 @@
+const greet = function () {
+ console.log("Hello!");
+};
+greet()
+
+// Functions stored in variables
+// Cannot be hoisted (you can’t call them before they’re defined)
---
+++ JavaScript/JS PRAC QUS/spread.js
@@ -0,0 +1,8 @@
+function multiply(a = 1, b = 1) {
+ return a * b;
+}
+function sum(...nums) {
+ return nums.reduce((acc, val) => acc + val, 0);
+}
+let nums = [1, 2, 3];
+console.log(sum(...nums)); // Spread
