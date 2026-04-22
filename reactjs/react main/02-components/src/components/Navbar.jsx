e466a7dbd364b49118430938b78cd7c57601c658
TAABISH ALAM <100014087+TAABISH379@users.noreply.github.com>
2026-04-22 22:02:56 +0530

    added react.js

---
+++ reactjs/react main/02-components/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className="nav">
+          Home
+          About Us
+          Pricing
+          Contact
+
+        </div>
+    </div>
+
+  )
+}
+
+export default Navbar
