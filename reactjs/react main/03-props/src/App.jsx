a21de4fb5de799ddfa425ddb98cd2dad49cb93b0
TAABISH ALAM <100014087+TAABISH379@users.noreply.github.com>
2026-05-03 16:09:24 +0530

    react props

--- reactjs/react main/03-props/src/App.jsx
+++ reactjs/react main/03-props/src/App.jsx
@@ -4,9 +4,11 @@ import Card from './components/Card'
 const App = () => {
   return (
     <div className='parent'>
-      <Card user='Mayuk' age={18} img='https://images.unsplash.com/photo-1751097006268-ae4601fcb117?q=80&w=1518&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
-      <Card user='Biswajit' age={28} img='https://images.unsplash.com/photo-1752801516481-cfb8c47ee9a3?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
-      <Card user='Rick' age={24} img='https://images.unsplash.com/photo-1751554933476-d029737d58b2?q=80&w=1160&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+      <Card user='User1' age={10} img='https://images.unsplash.com/photo-1751097006268-ae4601fcb117?q=80&w=1518&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' description='Hello, I am User1!' />
+      <Card user='User2' age={20} img='https://images.unsplash.com/photo-1751097006268-ae4601fcb117?q=80&w=1518&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' description='Hello, I am User2!' />
+      <Card user='User3' age={30} img='https://images.unsplash.com/photo-1751097006268-ae4601fcb117?q=80&w=1518&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' description='Hello, I am User3!' />
+      <Card user='User4' age={40} img='https://images.unsplash.com/photo-1751097006268-ae4601fcb117?q=80&w=1518&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' description='Hello, I am User4!' />
+      
     </div>
   )
 }
--- reactjs/react main/03-props/src/components/Card.jsx
+++ reactjs/react main/03-props/src/components/Card.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
 const Card = (props) => {
+  return (
+    <div className='card'>
+      <img src={props.img} alt="" />
+      <h1>{props.user}, {props.age}</h1>
+      <p>{props.description}</p>
+        <button>View Profile</button>
 
-    return (
-        <div className="card">
-            <img src={props.img} alt="" />
-            <h1>{props.user},{props.age}</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-            <button>View Profile</button>
-        </div>
-    )
+    </div>
+  )
 }
 
 export default Card
