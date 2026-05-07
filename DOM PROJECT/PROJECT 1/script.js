6aa649559ce7f1d7997f8dbc31417b2f022e0680
TAABISH ALAM <100014087+TAABISH379@users.noreply.github.com>
2026-05-07 19:17:25 +0530

    project1

--- DOM PROJECT/PROJECT 1/script.js
+++ DOM PROJECT/PROJECT 1/script.js
@@ -2,8 +2,15 @@ var istatus = document.querySelector("h5");
 
 var addFriend = document.querySelector("#add");
 
+var removeFriend = document.querySelector("#remove");
+
 addFriend.addEventListener("click", function(){
     istatus.innerHTML = "Friend Request Sent";
     istatus.style.color = "green";
-    
+
 });
+removeFriend.addEventListener("click", function(){
+    istatus.innerHTML = "Friend Request Removed";
+    istatus.style.color = "red";
+});
+
