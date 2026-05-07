4eb0d2017d8b3657ea3c3fefda47ace2a8ef59d3
TAABISH ALAM <100014087+TAABISH379@users.noreply.github.com>
2026-05-07 23:55:21 +0530

    project2

--- DOM PROJECT/PROJECT 2/script.js
+++ DOM PROJECT/PROJECT 2/script.js
@@ -3,5 +3,9 @@ var con=document.querySelector("#container")
 var love = document.querySelector("i")
 
 con.addEventListener("dblclick",function(){
-    console.log("double click")
+    love.style.transform = "translate(-50%, -50%) scale(1)";
+    setTimeout(function(){
+        love.style.transform = "translate(-50%, -50%) scale(0)";
+    }
+    ,1000)
 })
