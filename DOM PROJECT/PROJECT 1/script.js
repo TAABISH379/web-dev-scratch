a2da91bfe9e2a282dc4b9abcdb16707f68ec928f
TAABISH ALAM <100014087+TAABISH379@users.noreply.github.com>
2026-05-07 19:13:37 +0530

    project1

---
+++ DOM PROJECT/PROJECT 1/script.js
@@ -0,0 +1,7 @@
+var istatus = document.querySelector("h5");
+
+var addFriend = document.querySelector("#add");
+
+addFriend.addEventListener("click", function(){
+    istatus.innerHTML = "Friend Request Sent";
+});
